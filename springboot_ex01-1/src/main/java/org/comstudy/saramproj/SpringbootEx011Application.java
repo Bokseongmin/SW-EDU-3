7685bdf86831134da8f859e8946a0e89d0fcb20b
bokseongmin <bokseongmin@gmail.com>
2023-03-06 17:48:48 +0900

    test

---
+++ springboot_ex01-1/src/main/java/org/comstudy/saramproj/SpringbootEx011Application.java
@@ -0,0 +1,13 @@
+package org.comstudy.saramproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx011Application.class, args);
+	}
+
+}
