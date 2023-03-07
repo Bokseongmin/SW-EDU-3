3708048dba905a48665e7ff16191e2ea6fa3427d
bokseongmin <bokseongmin@gmail.com>
2023-03-07 17:34:04 +0900

    ex05_jpa_web 1th

---
+++ springboot_ex05_jpa_web/src/main/java/org/comstudy/boardweb/persistence/BoardRepository.java
@@ -0,0 +1,8 @@
+package org.comstudy.boardweb.persistence;
+
+import org.comstudy.boardweb.domain.Board;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+	
+}
